67e12fee04456c60682f986944d3b318ce60453e
sheva <yuansheva20@gmail.com>
2022-07-09 19:33:46 +0700

    featured

--- app.js
+++ app.js
@@ -45,16 +45,15 @@ const addCardComponent = (
     ? ''
     : `
         <div class="card">
-          ${
-            section === 'Project'
-              ? `<img
+          ${section === 'Project'
+      ? `<img
           src="${image}"
           class="card__image"
         /> <div class="card__actions">
         <a href="${link}" target="_blank" rel="noopener" class="card__actions__button">Learn More</a>
       </div>`
-              : ''
-          }
+      : ''
+    }
           <div class="card__header">
             <a href="${url || link}" target="_blank"
         rel="noopener noreferrer" class="card__header__title">${title}</a>
@@ -62,17 +61,16 @@ const addCardComponent = (
               <svg class="card__header__button__icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
             </button>
           </div>
-          <input class="card__input" type="text" name="link" id="link" value="${
-            url || link
-          }" hidden>
+          <p class="card__input" type="text" name="link" id="link" value="" hidden>${url || link
+    }</p> 
 
           <p class="card__body">
             ${body}
           </p>
           <ul class="card__tag">
             ${tag
-              ?.map((item) => `<li class="card__tag__item">${item}</li>`)
-              .join('')}
+      ?.map((item) => `<li class="card__tag__item">${item}</li>`)
+      .join('')}
           </ul>
         </div>
       `;
